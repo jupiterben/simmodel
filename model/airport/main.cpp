862f5d9f83cc101b388614b19a4de10993b8a464
jupiterben <jupiter.ben@live.com>
2014-02-15 16:03:23 +0800

    no message

--- model/airport/main.cpp
+++ model/airport/main.cpp
@@ -4,7 +4,11 @@
 int main()
 {
 	
+	Unit::quantity<double ,Unit::mass>  m1, m2;
+	Unit::quantity<double, Unit::time> t;
+	m1*t;
 
+	m1+m2;
 
 
 	return 1;
--- sim/cpputil.h
+++ sim/cpputil.h
@@ -13,11 +13,45 @@
 
 
 NSBEGIN(Unit)
+
 template <int N1, int N2, int N3, int N4, int N5, int N6, int N7>
 class dimension
 {
-
 };
+
+//
+template
+<
+	template<int N1, int N2,int N3,int N4,int N5,int N6,int N7> class D1,
+	template<int N1, int N2,int N3,int N4,int N5,int N6,int N7> class D2
+>
+struct dim_plus
+{
+	typedef dimension<D1::N1 + D2::N1
+					,D1::N2 + D2::N2
+					,D1::N3 + D2::N3
+					,D1::N4 + D2::N4
+					,D1::N5 + D2::N5
+					,D1::N6 + D2::N6
+					,D1::N7 + D2::N7> type;
+};
+
+////////////////////////////////////////////////////////////////////////////
+//template
+//<
+//template<int N1, int N2,int N3,int N4,int N5,int N6,int N7> class D1,
+//template<int N1, int N2,int N3,int N4,int N5,int N6,int N7> class D2
+//>
+//struct dim_minus
+//{
+//	typedef dimension<D1::N1 - D2::N1
+//		,D1::N2 - D2::N2
+//		,D1::N3 - D2::N3
+//		,D1::N4 - D2::N4
+//		,D1::N5 - D2::N5
+//		,D1::N6 - D2::N6
+//		,D1::N7 - D2::N7> type;
+//};
 
 
 
@@ -32,27 +66,42 @@ typedef dimension<0,0,0,0,0,0,1> angle;
 typedef dimension<0,0,0,0,0,0,0> scalar;
 
 template <typename T, typename D>
-class quantity
+class quantity 
 {
 public:
+	quantity():val(0){}
+	quantity(T d):val(d){ }
 	T value()const{ return val; }
 protected:
 	T val;
 };
 
-template <class T, class D>
+template <typename T, typename D>
 quantity<T,D>
 	operator+(quantity<T,D> x, quantity<T,D> y)
 {
 	return quantity<T,D>(x.value() + y.value());
 }
 
-template <class T, class D>
+template <typename T, typename D>
 quantity<T,D>
 	operator-(quantity<T,D> x, quantity<T,D> y)
 {
 	return quantity<T,D>(x.value() - y.value());
 }
+
+template<class T, typename D1, typename D2>
+quantity<T, typename dim_plus<typename D1,typename D2>::type > operator*(quantity<T,D1> x, quantity<T,D2> y)
+{
+	return quantity<T, dim_plus<D1,D2>::type > (x.value()*y.value());
+}
+
+//template<class T, class D1, class D2>
+//quantity<T, typename dim_minus<D1,D2>::type > operator/(quantity<T,D1> x, quantity<T,D2> y)
+//{
+//	return quantity<T,dim_minus<D1,D2>::type > (x.value()/y.value());
+//}
+
 
 
 NSEND
